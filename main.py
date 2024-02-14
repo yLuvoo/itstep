f91c4cf0f618825efd85f6df047a5700d8f73114
yLuvoo <glebpavlovschi@gmail.com>
2024-02-14 17:41:14 +0200

    First commit

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Hello!")
+
+# class Student:
+#     print("Hi!")
+#     def __init__(self):
+#         self.height = 160
+#         print("I am alive!")
+#
+# first_student = Student()
+
+# class Student:
+#     def __init__(self, height = 160):
+#         self.height = height
+#
+# nick = Student()
+# kate = Student(height=170)
+# kiril = Student(height=2033)
+# print(nick.height)
+# print(kate.height)
+# print(kiril.height)
